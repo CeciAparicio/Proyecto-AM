cf1557dcfc733a79718e377c051aade7d3bf1fcc
CeciAparicio <cecilia_aparicio_2396@hotmail.es>
2021-05-28 19:06:43 -0300

    Create proyecto.R

---
+++ proyecto.R
@@ -0,0 +1,52 @@
+rm(list=ls())
+
+library(dplyr)
+library(stringr)
+library(car)
+library(ggplot2)
+Energia1 <- read.delim("Energia1.txt") %>% 
+  arrange(PAISES)
+
+# valores para Uru en los años 1984,1991,1993,1994,1995, 1996.
+
+pais <- str_sub(Energia1$PAISES,1,-3)
+anio <- (str_sub(Energia1$PAISES,-2))
+anio<-paste(19,anio,sep="" ) %>% 
+  as.numeric()
+max(anio)
+min(anio)
+unique(pais) #hay paises duplicados
+
+for (i in 1:144) {
+ pais[i]= gsub(", ","_",pais[i])
+ pais[i]= gsub(" _","_",pais[i])
+ pais[i]= gsub("_", " ",pais[i])
+}
+
+unique(pais) #24 paises
+
+Energia1$Año<-anio
+Energia1$paises_n<-pais
+
+ggplot(Energia1 ,aes(Año,paises_n,fill=Prod_Ind))+
+  geom_tile(color= "white",size=0.1) +
+  scale_fill_viridis_c(name="Prod IND",option ="C")+
+  labs(title= ("PBI Industrial total"), x="Año", y="Países")+
+  scale_x_continuous(breaks =c(1984,1991,1993,1994,1995,1996))
+#  Perú es el país cuyo PBI indsutrial más aumentó en los 90. Leves aumentos en Argentina, Barbados por unos años,
+#  Uruguay. EN otros casos disminuyó (Jamaica, Ecuador)
+
+ggplot(Energia1 %>% filter(Año== 1996) ,aes(X.IMPORTA,ConsENpc,color=paises_n,size=PBIpc))+
+  geom_point()+
+  geom_label(aes(label=(paises_n)))+
+  labs(title="% Importado en oferta energética vs Consumo final pc", subtitle = "Año 1984")
+
+
+########## proyecto
+
+library(Rcmdr)
+library(FactoMineR)
+library(RcmdrPlugin.FactoMineR)
+library(Factoshiny)
+
+summary(Energia1) #todas las variables son cuantitativas
