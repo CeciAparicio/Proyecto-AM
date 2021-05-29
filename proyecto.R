c6f99e49f04a00cadf6369beca9ea8c60bbd257a
CeciAparicio <cecilia_aparicio_2396@hotmail.es>
2021-05-28 22:01:00 -0300

    Update proyecto.R

--- proyecto.R
+++ proyecto.R
@@ -1,5 +1,10 @@
 rm(list=ls())
+########## proyecto
 
+library(Rcmdr)
+library(FactoMineR)
+library(RcmdrPlugin.FactoMineR)
+library(Factoshiny)
 library(dplyr)
 library(stringr)
 library(car)
@@ -41,22 +46,28 @@ ggplot(Energia1 %>% filter(Año== 1996) ,aes(X.IMPORTA,ConsENpc,color=paises_n,s
   geom_label(aes(label=(paises_n)))+
   labs(title="% Importado en oferta energética vs Consumo final pc", subtitle = "Año 1984")
 
+rownames(Energia1)=Energia1$PAISES
 
-########## proyecto
-
-library(Rcmdr)
-library(FactoMineR)
-library(RcmdrPlugin.FactoMineR)
-library(Factoshiny)
+#### VARIABLES
 
 summary(Energia1) #todas las variables son cuantitativas -> lo más útil será ACP
 
 # Correlación 
 
-cor(Energia1[1:24,2:15]) # Correlaciones altas: 
-#Prod_Ind-X.C_IND 0.87181122; Prod_ENE-PBIpc 0.9268224 ; Prod_ENE-X.IMPORTA 0.82742916 ; 
-# ConsENpc-PBIpc 0.8105287 ; ConsENpc-O_PRIpc = 0.9702329 ; ConsENpc-O_SECpc 0.9032520 ; ConsENpc-CO2pc_cons -0.8348153  ; ConsENpc-X.C_SEC 0.9360467 ; ConsENpc-C_ELECpc 0.8597183 ;
-# PBIpc-Prod_Ene  0.9268224 ; PBIpc-O_SECpc 0.9591698; PBIpc- C_ELECpc 0.8218865 ;
-# O_PRIpc- O_SECpc  0.87450511 ; O_PRIpc-CO2_cons -0.80239813 ; O_PRIpc-X.C_SEC  -0.92102668 ; 
-# O_SECpc - X.C_SEC  0.8961128 ; O_SECpc-C_ELECpc  0.8164657 ;CO2pc - X.IMPORTA 0.81533895 ; X.C_RES-C_ELECpc -0.8940039; X.C_SEC -C_ELECpc 0.80593479
-# 
+cor(Energia1[Energia1$Año==1984,2:15]) # Correlaciones altas en 1984: 
+#Prod_Ene-PBIpc 0.83538542; ConsENpc-O_PRIpc 0.84860289 ;ConsENpc-C_ELEpc 0.80772690  ; O_PRIpc-C_ELECpc 0.8202470 
+
+cor(Energia1[Energia1$Año==1996,2:15]) # Correlaciones altas en 1996: 
+#Prod_Ene-PBIpc 0.810985198; ConsENpc-O_PRIpc 0.915940442  ;ConsENpc_C - X.C_IND 0.890504571  ; X-C_SEC-X.C_TRAN 0.85696498 
+#
+acp1984=PCA(Energia1[Energia1$Año==1984,c(2:15)])
+acp1996=PCA(Energia1[Energia1$Año==1996,c(2:15)])
+
+
+library(Factoshiny)
+resshiny = PCAshiny(res.pca1984)
+plot.PCA(res.PCA,choix='var',habillage = 'contrib',title="Gráfico perspectiva de variables")
+plot.PCA(res.PCA,habillage='contrib',title="Gráfico perspectiva de países",col.ind='#ED1536')
+summary(res.PCA) # las dos primeras dimensiones sólo aportan un 63.3% de la varianza, con 3 se explica 76.18%
+# ConENpc es explicada en 82.5%,81.2% porla primera dimension
+
